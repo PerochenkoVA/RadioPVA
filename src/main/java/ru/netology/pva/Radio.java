9b571b07e8fcb904a79061b34f996811e908516e
Uzbek150885 <Uzbek150885@gmail.com>
2021-08-05 08:41:39 +0300

    End task

--- src/main/java/ru/netology/pva/Radio.java
+++ src/main/java/ru/netology/pva/Radio.java
@@ -16,18 +16,71 @@ public class Radio {
     private int volDown;
     private boolean on;
     private String name = "Petrov";
-    private int volume1;
-    private int volume2;
 
     public Radio() {
     }
 
-    public Radio(int maxVolume, int minVolume) {
-        this.maxVolume = maxVolume;
-        this.minVolume = minVolume;
-    }
+    public Radio (int numberStation, int statNumber){
 
-    public Radio(int numberStation, int volume, int maxVolume, int minVolume, int maxStation, int minStation, int statNumber, int volUp, int volDown, boolean on, String name) {
+        numberStation = numberStation+statNumber;
+        if (numberStation>maxStation){
+            numberStation=minStation;
+        }
+        if (numberStation<minStation){
+            numberStation=maxStation;
+        }
+        this.numberStation = numberStation;
+    }
+        public int getNumberStation() {return numberStation;}
+
+    //public void setNumberStation(int numberStation) {this.numberStation = numberStation;}
+    public int getVolume() {return volume;}
+   // public void setVolume(int volume) {this.volume = volume;}
+    public int getMaxVolume() {return maxVolume;}
+  //  public void setMaxVolume(int maxVolume) {this.maxVolume = maxVolume;}
+    public int getMinVolume() {return minVolume;}
+  //  public void setMinVolume(int minVolume) {this.minVolume = minVolume;}
+    public int getStatNumber() {return statNumber;}
+   // public void setStatNumber(int statNumber) {this.statNumber = statNumber;}
+    public int getVolUp() {return volUp;}
+   // public void setVolUp(int volUp) {this.volUp = volUp;}
+    public int getVolDown() {return volDown;}
+   // public void setVolDown(int volDown) {this.volDown = volDown;}
+    public boolean isOn() {return on;}
+   // public void setOn(boolean on) {this.on = on;}
+    public String getName() {return name;}
+   // public void setId(String name) {this.name = name;}
+    public int getMaxStation() {return maxStation;}
+   // public void setMaxStation(int maxStation) {this.maxStation = maxStation;}
+    public int getMinStation() {return minStation;}
+    //public void setMinStation(int minStation) {this.minStation = minStation;}
+}
+
+
+   /* //Регулировка громкости по максимуму (с 10 на 1)
+    public void setVolUp(int volUp) {
+        if (volUp < 10) {
+            volUp = volUp + 1;
+        }
+        if (volUp > 10) {
+            volUp = 1;
+        }
+        this.volUp = volUp;
+    }
+    //Регулировка громкости по минимуму (с 1 на 10)
+    public void setVolDown(int volDown) {
+        if (volDown < 10) {
+            volDown = volDown - 1;
+        }
+        if (volDown < 1) {
+            volDown = 10;
+        }
+        this.volDown = volDown;
+    }
+*/
+
+/*
+public Radio(int numberStation, int volume, int maxVolume, int minVolume, int maxStation, int minStation, int statNumber, int volUp, int volDown, boolean on, String name) {
         this.numberStation = numberStation;
         this.volume = volume;
         this.maxVolume = maxVolume;
@@ -62,101 +115,6 @@ public class Radio {
         if (volume2 < minVolume) {
             volume2 = maxVolume;
         }
-    }
-
-    public int getNumberStation() {
-        return numberStation;
-    }
-    public void setNumberStation(int numberStation) {
-        this.numberStation = numberStation;
-    }
-    public int getVolume() {
-        return volume;
-    }
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
-    public int getMaxVolume() {
-        return maxVolume;
-    }
-    public void setMaxVolume(int maxVolume) {
-        this.maxVolume = maxVolume;
-    }
-    public int getMinVolume() {
-        return minVolume;
-    }
-    public void setMinVolume(int minVolume) {
-        this.minVolume = minVolume;
-    }
-    public int getStatNumber() {
-        return statNumber;
-    }
-    public void setStatNumber(int statNumber) {
-        this.statNumber = statNumber;
-    }
-    public int getVolUp() {
-        return volUp;
-    }
-    public void setVolUp(int volUp) {
-        this.volUp = volUp;
-    }
-    public int getVolDown() {
-        return volDown;
-    }
-    public void setVolDown(int volDown) {
-        this.volDown = volDown;
-    }
-    public boolean isOn() {
-        return on;
-    }
-    public void setOn(boolean on) {
-        this.on = on;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setId(String name) {
-        this.name = name;
-    }
-    public int getMaxStation() {
-        return maxStation;
-    }
-    public void setMaxStation(int maxStation) {
-        this.maxStation = maxStation;
-    }
-    public int getMinStation() {
-        return minStation;
-    }
-    public void setMinStation(int minStation) {
-        this.minStation = minStation;
-    }
-    public void setName(String name) {this.name = name;}
-    public int getVolume1() {return volume1;}
-    public void setVolume1(int volume1) {this.volume1 = volume1;}
-    public int getVolume2() {return volume2;}
-    public void setVolume2(int volume2) {this.volume2 = volume2;}
-}
-
-
-   /* //Регулировка громкости по максимуму (с 10 на 1)
-    public void setVolUp(int volUp) {
-        if (volUp < 10) {
-            volUp = volUp + 1;
-        }
-        if (volUp > 10) {
-            volUp = 1;
-        }
-        this.volUp = volUp;
-    }
-    //Регулировка громкости по минимуму (с 1 на 10)
-    public void setVolDown(int volDown) {
-        if (volDown < 10) {
-            volDown = volDown - 1;
-        }
-        if (volDown < 1) {
-            volDown = 10;
-        }
-        this.volDown = volDown;
     }
 */
 
--- src/test/java/ru/netology/pva/RadioTest.java
+++ src/test/java/ru/netology/pva/RadioTest.java
@@ -8,12 +8,42 @@ class RadioTest {
     @Test
     public void shouldUseDefaultConstructor() {
         Radio radio = new Radio();
+        assertEquals(0, radio.getNumberStation());
+        assertEquals(0, radio.getVolume());
+        assertEquals(10, radio.getMaxVolume());
+        assertEquals(2, radio.getMinVolume());
+        assertEquals(10, radio.getMaxStation());
+        assertEquals(0, radio.getMinStation());
+        assertEquals(0, radio.getStatNumber());
+        assertEquals(0, radio.getVolUp());
+        assertEquals(0, radio.getVolDown());
+        assertEquals(false, radio.isOn());
         assertEquals("Petrov", radio.getName());
     }
 
+
+
+    @Test
+    public void RadioStationUp() {
+        Radio radio = new Radio(10,1);
+
+        assertEquals(0,radio.getNumberStation());
+    }
+    @Test
+    public void RadioStationDown(){
+        Radio radio= new Radio(0,-1);
+
+        assertEquals(10, radio.getNumberStation());
+    }
+
+
+}
+
+
+/*
     @Test
     public void shouldUsedConstructorAllParametrRadio() {
-        Radio radio = new Radio(3, 2, 50, 1, 9, 1, 4, 1, 1, true, "PetrovBM");
+        Radio radio = new Radio();
 
         assertEquals(3, radio.getNumberStation());
         assertEquals(2, radio.getVolume());
@@ -26,8 +56,10 @@ class RadioTest {
         assertEquals(true, radio.isOn());
         assertEquals("PetrovBM", radio.getName());
     }
+}
+*/
 
-    @Test
+   /* @Test
     public void shouldUsedConstructorRadioVolume100() {
         Radio radio = new Radio(100, 0);
 
@@ -48,7 +80,7 @@ class RadioTest {
 
     }
 
-}
+}*/
 
     /*@Test
     public void setMoreMaximum() {
